6af19a9048afc830a187ca10b67076400a3cc8e7
murtidip <dipanshusharma5334@gmail.com>
2026-03-18 22:04:06 +0530

    dee

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,7 +8,7 @@ import PricingPlansDashboard from './Component/Admin/PricingPlansDashboard';
 
 function App() {
   return (
-    <Router basename="/admin">
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<AdminPanel />} />
